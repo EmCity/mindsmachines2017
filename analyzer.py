c692b19c6b1ec28d6d2e90190e659d862d25eff1
Lukas Mohs <ga47boq@mytum.de>
2017-06-12 15:34:29 +0200

    added date conversion

--- analyzer.py
+++ analyzer.py
@@ -1,14 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model
+from sklearn import datasets, linear_model, model_selection
+
 #with open('train.csv') as csvfile:
 #	reader = csv.reader(csvfile, delimiter=';', quotechar="'")
 #
 #	for row in reader:
 #		print(row[1])
 
+time_columns = ['date_reception_OMP','date_besoin_client',
+	'date_transmission_proc','date_emission_commande','date_prev_livraison_prog',
+	'date_reelle_livraison_prog','date_reelle_livraison_indus','date_prev_fin_usinage',
+	'date_reelle_fin_usinage','date_prev_fin_TS','date_reelle_fin_TS','date_expedition',
+	'date_livraison_contractuelle','date_livraison_previsionnelle_S','date_reception_effective',
+	'date_livraison_contractuelle_initiale','date_liberation','date_affectation']
+
 import pandas as pd
-df = pd.read_csv('data/train.csv')
+df = pd.read_csv('data/train.csv',  parse_dates=time_columns)
+
 
 ## Iterate over all columns and drop sparse ones
 for column in df:
@@ -28,7 +37,25 @@ for column in df:
         df[column].fillna(most_common)
         print("replaced nans with: " + str(most_common))
 
-#wirte to csv
+#convert timestamps
+
+
+for column in time_columns:
+	df[column] = pd.to_datetime(df[column], unit='s')
+	#for in range(len(df)):
 
 df.to_csv("cleaned_output.csv", sep=";")
+
+#wirte to csv
+y = df['date_reception_OMP']
+X = df.drop('date_reception_OMP',axis=1)
+
+
+#model_selection.TimeSeriesSplit
+X_train, X_test, y_train, y_test = model_selection.train_test_split( X, y, test_size=0.8)
+
+regr = linear_model.LinearRegression()
+regr.fit(X_train,y_train)
+print(str(regr.score(X_test,y_test)))
+
 
