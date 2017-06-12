8431c393bba60601b46643bedf9b64a144754387
Florian Schaule <f.schaule@gmail.com>
2017-06-13 00:51:14 +0200

    update

--- prediction.py
+++ prediction.py
@@ -18,12 +18,16 @@ for column in df:
 
 y = df['total_cycle_duration_new']
 X = df.drop('total_cycle_duration_new', axis=1)
+X = X.drop('date_liberation_new', axis=1)
+X = X.drop('date_reception_OMP_new', axis=1)
 
 # model_selection.TimeSeriesSplit
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
 
 regr = linear_model.LinearRegression()
 regr.fit(X_train, y_train)
+for i in range(0, len(regr.coef_)):
+    print(str(X_train.columns.values.tolist()[i]) + " " + str(regr.coef_[i]))
 pred_result = regr.predict(X_test)
 
 result_df  = pd.concat([X_test, y_test], axis=1)
@@ -40,7 +44,17 @@ result_df.to_csv("results/result_only.csv", sep=',')
 print("linear regression " + str(regr.score(X_test, y_test)))
 print("linear regression " + str(math.sqrt(metrics.mean_squared_error(y_test, regr.predict(X_test)))))
 
-# svr = svm.SVR(kernel='poly')
+lasso = linear_model.Lasso()
+lasso.fit(X_train, y_train)
+print("lasso regression " + str(lasso.score(X_test, y_test)))
+print("lasso regression " + str(math.sqrt(metrics.mean_squared_error(y_test, lasso.predict(X_test)))))
+
+elastic = linear_model.ElasticNet()
+elastic.fit(X_train, y_train)
+print("elastic regression " + str(elastic.score(X_test, y_test)))
+print("elastic regression " + str(math.sqrt(metrics.mean_squared_error(y_test, elastic.predict(X_test)))))
+
+# svr = svm.SVR(kernel='linear')
 # svr.fit(X_train, y_train)
 # print("svr regression " + str(svr.score(X_test, y_test)))
 # print("svr regression " + str(math.sqrt(metrics.mean_squared_error(y_test, svr.predict(X_test)))))
