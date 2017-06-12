dde17a754a59e5532732f63076b9cabe2bcc770a
Sebastian Wagner <sebastiantimwagner@gmail.com>
2017-06-12 18:33:57 +0200

    Added frontend.

---
+++ frontend/viz.js
@@ -0,0 +1,10 @@
+d3.csv("train.csv", function(d) {
+  return {
+    city : d.city,
+    state : d.state,
+    population : +d.population,
+    land_area : +d["land area"]
+  };
+}, function(data) {
+  console.log(data[0]);
+});
